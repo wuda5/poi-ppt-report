026c8896ff8371c92e59a4ff5d0a9cdd3177c7c5
wuda5 <2252316187@qq.com>
2020-03-07 11:32:06 +0800

    poi--生成ppt--demo--test

---
+++ src/main/java/com/wu/SpringbootMybatisApplication.java
@@ -0,0 +1,17 @@
+package com.wu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisApplication.class, args);
+		System.out.println(9999);
+		System.out.println(9999);
+		System.out.println(9999);
+
+	}
+
+}
---
+++ src/main/java/com/wu/domain/User.java
@@ -0,0 +1,14 @@
+package com.wu.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+
+
+}
